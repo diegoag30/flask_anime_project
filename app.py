a8283926b3d61a3b5e7585a7b64721451f0628aa
diegoag30 <diego.pablo.aguilar@gmail.com>
2021-07-22 23:42:56 -0400

    home page connected to api

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, url_for
+from requests import get
 app = Flask(__name__)
 
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    animes = get('https://kitsu.io/api/edge/anime?filter[seasonYear]=2020').json()
+    animes = animes['data']
+    titles =[]
+    for anime in animes:
+        try:
+            titles.append(anime['attributes']['titles']['en_jp'])
+        except KeyError:
+            pass
+
+    ## return animes['data'][0]['attributes']['titles']['en_jp']
+    return render_template('index.html',anime_titles = titles)
+
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True,)
