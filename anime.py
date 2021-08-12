cb85e368df315782f2923ac342fce478b79f648e
diegoag30 <diego.pablo.aguilar@gmail.com>
2021-08-12 00:49:11 -0400

    added anime class to handle relavant anime data

---
+++ anime.py
@@ -0,0 +1,8 @@
+class Anime():
+    # Simple class that handles relevant data from an anime
+    def __init__(self,name,image,status,chapters_count):
+        self.name = name
+        self.image = image
+        self.status = status
+        self.chapters_count = chapters_count
+    
--- app.py
+++ app.py
@@ -1,20 +1,31 @@
+from anime import Anime
 from flask import Flask, render_template, url_for
 from requests import get
+from anime import Anime
+
 app = Flask(__name__)
 
 @app.route('/home')
 def index():
+    
+    # Connection to anime api
     animes = get('https://kitsu.io/api/edge/anime?filter[seasonYear]=2020').json()
-    animes = animes['data']
-    titles =[]
-    for anime in animes:
+    anime_info = animes['data']
+    anime_list = []
+    for anime in anime_info:
         try:
-            titles.append(anime['attributes']['titles']['en_jp'])
+            # Anime Objects creation
+            current_anime = Anime(
+            anime['attributes']['titles']['en_jp'],
+            anime['attributes']['posterImage']['medium'],
+            anime['attributes']['status'],
+            anime['attributes']['episodeCount']
+            )
+            anime_list.append(current_anime)
         except KeyError:
             pass
 
-    ## return animes['data'][0]['attributes']['titles']['en_jp']
-    return render_template('index.html',anime_titles = titles)
+    return render_template('index.html',anime_titles = anime_list)
 
 
 if __name__== "__main__":
